561a40d375b5cef6ecb5503b58dd8cb837055158
Iago Leal de Freitas <iago.lealf@gmail.com>
2020-05-17 19:38:46 -0300

    Conjugate gradient: additional maxiter parameter

--- src/ConjugateGradient.jl
+++ src/ConjugateGradient.jl
@@ -1,5 +1,5 @@
 """
-    conjugate_gradient(A, b, inner; atol, x0)
+    conjugate_gradient(A, b, inner; maxiters, atol, x0)
 
 __Iteratively__ solves the linear equation `A(z) = b`,
 where `A` is a linear operator and `b` an element of an abstract vector space.
@@ -13,6 +13,7 @@ The optional parameters are:
     - `x0` is the initial value for the iterative method and defaults to zero.
     -  `atol` is the absolute tolerance of the stopping criterion.
         The method runs while the squared norm of the residue is larger than `atol`.
+    - `maxiters` is the maximum number of iterations to do before stopping. Default is infinity.
 
 Requirements:
     - `A` must be self-adjoint with respect to `inner`;
@@ -20,7 +21,16 @@ Requirements:
     - If given, `x0` must be in the kernel of `A`, i.e. `A(x0) == zero(x0)`.
 
 """
-function conjugate_gradient(A, b, inner; atol=1e-6, x0=nothing)
+function conjugate_gradient(A, b, inner; maxiters=nothing,
+                                         atol=1e-6,
+                                         x0=nothing)
+    # Set number of iterations
+    if isnothing(maxiters)
+        iter = Iterators.cycle(1)
+    else
+        iter = 1:maxiters
+    end
+    # Set initial values
     if isnothing(x0)
         x = zero(b)
         r = b
@@ -30,12 +40,14 @@ function conjugate_gradient(A, b, inner; atol=1e-6, x0=nothing)
     end
     p = r
     norm_r_prev = inner(r,r)
-    while norm_r_prev > atol
+    for n in iter
         Ap    = A(p)
         alpha = norm_r_prev / inner(p, Ap)
         x     = x + alpha * p
         r     = r - alpha * Ap
         norm_r_new = inner(r,r)
+        sqrt(norm_r_new) < atol && break
+
         beta  = norm_r_new / norm_r_prev
         p     = r + beta * p
         norm_r_prev = norm_r_new
